903750f60cd2452a23e650b943b28098cb499cd3
ABBOU Antoine <aabbou@g-a101-13.local>
2018-02-12 17:40:08 +0100

    refactoring

--- app/src/main/java/com/example/pibbou/afca/ui/list/FragmentOne.kt
+++ app/src/main/java/com/example/pibbou/afca/ui/list/FragmentOne.kt
@@ -4,33 +4,116 @@ package com.example.pibbou.afca.ui.list
  * Created by antoineabbou on 11/01/2018.
  */
 
-import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.RelativeLayout
-import android.widget.TextView
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+import com.example.pibbou.afca.App
 import com.example.pibbou.afca.R
-
+import com.example.pibbou.afca.repository.entity.Event
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class FragmentOne : Fragment() {
 
-    var tv_name: TextView? = null
-    var rel_main: RelativeLayout? = null
+    /*var tv_name: TextView? = null
+    var rel_main: RelativeLayout? = null*/
+    private var view1: View? = null
+    // Prepare eventsByDay array
+    private var events: ArrayList<Event>? = ArrayList()
 
-
+    private var recycler_view_category_list: RecyclerView? = null
+    private var categoriesAdapter: CategoryListAdapter? = null
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        var view: View? = inflater!!.inflate(R.layout.fragment_layout, container, false)
 
-        rel_main=view?.findViewById<RelativeLayout>(R.id.rel_main) as RelativeLayout
+        view1  = inflater!!.inflate(R.layout.fragment_one, container, false)
+
+        /*rel_main=view?.findViewById<RelativeLayout>(R.id.rel_main) as RelativeLayout
         rel_main?.setBackgroundColor(Color.CYAN)
         tv_name = view?.findViewById<TextView>(R.id.tv_name) as TextView
-        tv_name?.text = "W3Adda First Tab"
+        tv_name?.text = "Hello"*/
 
 
-        return view
+        setupEventsList()
+        setupDay()
+
+        return view1
+    }
+
+    private fun setupEventsList() {
+        // TODO: REMOVE THIS METHOD IN SETUPEVENTSLIST
+
+        // Get recyclerview View
+        recycler_view_category_list = view1!!.findViewById<View>(R.id.recycler_view_category_list) as RecyclerView
+
+        // Set fixed size
+        recycler_view_category_list!!.setHasFixedSize(true)
+
+        // Prepare adapter
+        categoriesAdapter = CategoryListAdapter(this.context, events)
+
+        recycler_view_category_list!!.layoutManager = LinearLayoutManager(this.context, LinearLayoutManager.VERTICAL, false)
+
+        // Set adapter
+        recycler_view_category_list!!.adapter = categoriesAdapter
+    }
+
+
+    private fun setupEventDatas(day: Int) {
+
+        // TODO: https://stackoverflow.com/questions/31367599/how-to-update-recyclerview-adapter-data - UPDATE ADAPTER
+
+        events!!.clear()
+
+        // Get datarepo
+        val dataRepository = App.sInstance!!.getDataRepository()
+        // Thanks to datarepo get all events
+        val eventsByDay = dataRepository!!.getEventsByDay(day)
+
+        for(event in eventsByDay!!){
+            events!!.add(event)
+        }
+        categoriesAdapter!!.notifyDataSetChanged()
+    }
+
+
+    private fun setupDay() {
+
+        val adapter = ArrayAdapter.createFromResource(
+                this.context,
+                R.array.sort_day,
+                android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        val spinner = view1!!.findViewById<View>(R.id.spinner) as Spinner
+
+        spinner.adapter = adapter
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+
+            override fun onItemSelected(adapterView: AdapterView<*>, view: View?, position: Int, id: Long) {
+
+                val item = adapterView.getItemAtPosition(position)
+                if (item != null) {
+                    /*Toast.makeText(this@MainActivity, item.toString(),
+                            Toast.LENGTH_SHORT).show()*/
+                    // Setup DATAS
+                    setupEventDatas(position)
+                }
+            }
+
+            override fun onNothingSelected(adapterView: AdapterView<*>) {
+                // TODO Auto-generated method stub
+
+            }
+        }
+
     }
 }
--- app/src/main/java/com/example/pibbou/afca/ui/list/MainActivity.kt
+++ app/src/main/java/com/example/pibbou/afca/ui/list/MainActivity.kt
@@ -3,26 +3,10 @@ package com.example.pibbou.afca.ui.list
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.pibbou.afca.R
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-import android.view.View
-import com.example.pibbou.afca.App
-import com.example.pibbou.afca.repository.entity.Category
-import com.example.pibbou.afca.R.id.spinner
-import android.R.array
-import android.widget.*
-import com.example.pibbou.afca.repository.entity.Event
-import android.widget.AdapterView.OnItemSelectedListener
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-
-    // Prepare eventsByDay array
-    private var events: ArrayList<Event>? = ArrayList()
-
-    private var recycler_view_category_list: RecyclerView? = null
-    private var categoriesAdapter: CategoryListAdapter? = null
 
     internal lateinit var viewpageradapter:ViewPagerAdapter //Declare PagerAdapter
     private val tabIcons = Array(3) {
@@ -40,83 +24,8 @@ class MainActivity : AppCompatActivity() {
         this.viewPager.adapter=viewpageradapter  //Binding PagerAdapter with ViewPager
         this.tab_layout.setupWithViewPager(this.viewPager) //Binding ViewPager with TabLayout
         setupTabIcons() // Put icons inside tab layout --> Need real ones
-
-        setupEventsList()
-        setupDay()
-
     }
 
-
-
-    private fun setupEventsList() {
-        // TODO: REMOVE THIS METHOD IN SETUPEVENTSLIST
-
-        // Get recyclerview View
-        recycler_view_category_list = findViewById<View>(R.id.recycler_view_category_list) as RecyclerView
-
-        // Set fixed size
-        recycler_view_category_list!!.setHasFixedSize(true)
-
-        // Prepare adapter
-        categoriesAdapter = CategoryListAdapter(this, events)
-
-        recycler_view_category_list!!.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-
-        // Set adapter
-        recycler_view_category_list!!.adapter = categoriesAdapter
-    }
-
-
-    private fun setupEventDatas(day: Int) {
-
-        // TODO: https://stackoverflow.com/questions/31367599/how-to-update-recyclerview-adapter-data - UPDATE ADAPTER
-
-        events!!.clear()
-
-        // Get datarepo
-        val dataRepository = App.sInstance!!.getDataRepository()
-        // Thanks to datarepo get all events
-        val eventsByDay = dataRepository!!.getEventsByDay(day)
-
-        for(event in eventsByDay!!){
-            events!!.add(event)
-        }
-        categoriesAdapter!!.notifyDataSetChanged()
-    }
-
-
-    private fun setupDay() {
-
-        val adapter = ArrayAdapter.createFromResource(
-                this,
-                R.array.sort_day,
-                android.R.layout.simple_spinner_item)
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
-        val spinner = findViewById<View>(R.id.spinner) as Spinner
-
-        spinner.adapter = adapter
-
-        spinner.onItemSelectedListener = object : OnItemSelectedListener {
-
-            override fun onItemSelected(adapterView: AdapterView<*>, view: View, position: Int, id: Long) {
-
-                val item = adapterView.getItemAtPosition(position)
-                if (item != null) {
-                    Toast.makeText(this@MainActivity, item.toString(),
-                            Toast.LENGTH_SHORT).show()
-                    // Setup DATAS
-                    setupEventDatas(position)
-                }
-            }
-
-            override fun onNothingSelected(adapterView: AdapterView<*>) {
-                // TODO Auto-generated method stub
-
-            }
-        }
-
-    }
 
     private fun setupTabIcons() {
         tab_layout.getTabAt(0)?.setIcon(tabIcons[0])
